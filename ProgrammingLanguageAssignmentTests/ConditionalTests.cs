8ac0ad7a450c03e220442bded8d403b12c4b8a90
Navnidhi2475 <148339271+Navnidhi2475@users.noreply.github.com>
2023-11-15 20:26:06 +0000

    test Files
    test Files

---
+++ ProgrammingLanguageAssignmentTests/ConditionalTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingLanguageAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageAssignment.Tests
+{
+    [TestClass()]
+
+    public class ConditionalTests
+    {
+
+        /// <summary>
+        /// This method checks if the specified condition is false
+        /// </summary>
+        [TestMethod()]
+        public void ConditionalTest()
+        {
+            int value = 10;
+            int variable = 5;
+            string operatorValue = ">";
+            bool If = true;
+
+            Conditional conditional = new Conditional();
+            conditional.set(If, variable, value, operatorValue);
+
+            Assert.IsFalse(conditional.checkIfLogic());
+
+        }
+    }
+}
---
+++ ProgrammingLanguageAssignmentTests/VariableTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingLanguageAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageAssignment.Tests
+{
+    [TestClass()]
+    public class VariableTests
+    {
+        [TestMethod()]
+        public void VariableTest()
+        {
+            int value = 10;
+            string name = "size";
+            int index = 0;
+
+            Variable variable = new Variable();
+            variable.set(value, name, index);
+
+            Assert.IsTrue(variable.getVariableValue().Contains(value));
+            Assert.IsTrue(variable.getVariableName().Contains(name));
+
+        }
+    }
+}
